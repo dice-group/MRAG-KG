1a043828ea998db8a7921eb1baf7253578a396fa
Alkid <alkid1baci@gmail.com>
2024-10-29 19:58:35 +0100

    added/updated question generation scripts

---
+++ general_working_directory/question_fragmentation_generation.py
@@ -0,0 +1,58 @@
+import json
+import time
+from openai import OpenAI
+
+
+def get_message(query):
+    client = OpenAI(base_url="http://tentris-ml.cs.upb.de:8501/v1", api_key="token-tentris-upb")
+    return client.chat.completions.create(
+        model="tentris",
+        messages=[
+            {
+                "role": "user",
+                "content":
+                    [
+                        {
+                            "type": "text",
+                            "text": "Separate each part of the following question into self-contained questions. Separate them in new line without using ordering numbers. Here is the question:"
+                                    f"{query}"
+                        },
+                    ]
+            }
+        ],
+        temperature=0.1,
+        seed=1
+    ).choices[0].message.content
+
+
+image_full_question_fragments_dict = dict()
+
+
+def start_generation():
+    count = 0
+    with open('questions.json') as json_file:
+        data = json.load(json_file)
+
+    images_iris = data.keys()
+
+    for image in images_iris:
+        separated_questions = get_message(data[image])
+        image_full_question_fragments_dict[image] = separated_questions
+        count += 1
+        print(f"{image}: {count:,}/45,623")
+        if count > 10:
+            break
+
+    with open("fragmented_questions.json", "w") as outfile:
+        json.dump(image_full_question_fragments_dict, outfile)
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    try:
+        start_generation()
+    except Exception as e:
+        print(e)
+        with open("fragmented_questions_uncompleted.json", "w") as outfile:
+            json.dump(image_full_question_fragments_dict, outfile)
+    print(time.time() - start_time)
--- general_working_directory/question_generation.py
+++ general_working_directory/question_generation.py
@@ -1,20 +1,17 @@
 import base64
-
-import aiohttp
-import asyncio
+import json
 import time
 from openai import OpenAI
 from owlapy.iri import IRI
 from owlapy.owl_ontology_manager import OntologyManager
 from owlapy.owl_property import OWLDataProperty
 from owlapy.owl_reasoner import OntologyReasoner, FastInstanceCheckerReasoner
-from rdflib import Graph, URIRef, Literal, BNode, RDFS, OWL, Namespace, RDF
-from rdflib.namespace import XSD
-from owlapy.owl_individual import OWLNamedIndividual
+
 
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
+
 
 manager = OntologyManager()
 ontology = manager.load_ontology(IRI.create("file://fashionpedia-third-generation.owl"))
@@ -23,48 +20,70 @@ reasoner = FastInstanceCheckerReasoner(base_reasoner=base_reasoner, ontology=ont
 dprop1 = OWLDataProperty(IRI.create("http://example.org/hasFileName"))
 dprop2 = OWLDataProperty(IRI.create("http://example.org/hasDescription"))
 dprop3 = OWLDataProperty(IRI.create("http://example.org/hasLLMDescription"))
-image_iri_as_str = "http://example.org/image_25521"
-image_ind = OWLNamedIndividual(image_iri_as_str)
-
-image_filename = "images/" + str(list(reasoner.data_property_values(image_ind, dprop1))[0].get_literal())
-llm_description = str(list(reasoner.data_property_values(image_ind, dprop3))[0].get_literal())
-base64_image = encode_image(image_filename)
-all_descriptions = ""
-
-# "Consider you are a user that is looking for clothes and other apparels in an online recommandation system."
-#                             "Formulate a query of a prompt-like structure that the user would use in such a way that the attached image would be returned. To generate the query you can take in consideration the following auxiliary information about the image:"
-#                             f"{all_descriptions}"
-#                             f"{llm_description}"
-
-# "Only write the query which should be a question and always end with a questionmark."
-for d in list(reasoner.data_property_values(image_ind,dprop2)):
-    all_descriptions = all_descriptions + d.get_literal() + "\n"
 
 client = OpenAI(base_url="http://tentris-ml.cs.upb.de:8501/v1", api_key="token-tentris-upb")
-print(client.chat.completions.create(
-    model="tentris",
-    messages=[
-        {
-        "role": "user",
-        "content":
-            [
-                {
-                    "type": "text",
-                    "text": "Consider you are a user that is looking for clothes/apparels in an online recommandation system."
-                            "Formulate a query of a prompt-like structure that the you would ask in such a way that the attached image would be recommended to you. To generate the query you can take in consideration the following auxiliary information about the image:"
-                            f"{all_descriptions}"
-                            f"{llm_description}" 
-                            "Only write the query which should be a question and always end with a questionmark."
-                },
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{base64_image}"
-                    }
-                }
-            ]
-        }
-    ],
-    temperature=0.1,
-    seed=1
-).choices[0].message.content)
+
+
+def get_message(base64, desc, llm_desc):
+    return client.chat.completions.create(
+        model="tentris",
+        messages=[
+            {
+                "role": "user",
+                "content":
+                    [
+                        {
+                            "type": "text",
+                            "text": "Consider you are a user that is looking for clothes/apparels in an online recommandation system."
+                                    "Formulate a query of a prompt-like structure that the you would ask in such a way that the attached image would be recommended to you. To generate the query you can take in consideration the following auxiliary information about the image:"
+                                    f"{desc}"
+                                    f"{llm_desc}"
+                                    "Only write the query which should be a question and always end with a questionmark."
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{base64}"
+                            }
+                        }
+                    ]
+            }
+        ],
+        temperature=0.1,
+        seed=1
+    ).choices[0].message.content
+
+
+image_full_question_dict = dict()
+
+
+def start_generation():
+    count = 0
+    for image_ind in ontology.individuals_in_signature():
+        image_filename = "images/" + str(list(reasoner.data_property_values(image_ind, dprop1))[0].get_literal())
+        base64_image = encode_image(image_filename)
+        llm_description = str(list(reasoner.data_property_values(image_ind, dprop3))[0].get_literal())
+        if len(llm_description) > 4000:
+            llm_description = llm_description[:4000]
+        all_descriptions = ""
+        for d in list(reasoner.data_property_values(image_ind, dprop2)):
+            all_descriptions = all_descriptions + d.get_literal() + "\n"
+        question = get_message(base64_image, all_descriptions, llm_description)
+        image_iri = image_ind.str
+        image_full_question_dict[image_iri] = question
+        count += 1
+        print(f"{image_iri}: {count:,}/45,623")
+
+    with open("questions.json", "w") as outfile:
+        json.dump(image_full_question_dict, outfile)
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    try:
+        start_generation()
+    except Exception as e:
+        print(e)
+        with open("questions_uncompleted.json", "w") as outfile:
+            json.dump(image_full_question_dict, outfile)
+    print(time.time() - start_time)
