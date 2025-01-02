3bda2ad76f745a91b70f68ff514af48c75c8e026
Alkid <alkid1baci@gmail.com>
2025-01-02 19:26:06 +0100

    added evaluation results for second retrieval method and renamed other evaluation results files

--- general_working_directory/mrr_and_hits@k.py
+++ general_working_directory/mrr_and_hits@k.py
@@ -25,7 +25,7 @@ k3 = 50
 k4 = 100
 
 print("==== embedding-based model b1====/n")
-with open('evaluation_results_embedding-based_on_second_KG.json', 'r') as file:
+with open('evaluation_results_embedding-based_KG2_b1.json.json', 'r') as file:
     data = json.load(file)
 
 print(f"Second KG MRR = {find_mrr(data)}")
@@ -34,7 +34,7 @@ print(f"Second KG Hits@{k2} = {find_hit_k(data, k2)}")
 print(f"Second KG Hits@{k3} = {find_hit_k(data, k3)}")
 print(f"Second KG Hits@{k4} = {find_hit_k(data, k4)}")
 
-with open('evaluation_results_embedding-based_on_third_KG.json', 'r') as file:
+with open('evaluation_results_embedding-based_KG3_b1.json.json', 'r') as file:
     data = json.load(file)
 
 print(f"Third KG MRR = {find_mrr(data)}")
@@ -43,8 +43,8 @@ print(f"Third KG Hits@{k2} = {find_hit_k(data, k2)}")
 print(f"Third KG Hits@{k3} = {find_hit_k(data, k3)}")
 print(f"Third KG Hits@{k4} = {find_hit_k(data, k4)}")
 
-print("==== BM25 model b1====/n")
-with open('evaluation_results_bm25_on_second_KG.json', 'r') as file:
+print("==== BM25 model on first benchmark ====/n")
+with open('evaluation_results_bm25_KG2_b1.json', 'r') as file:
     data = json.load(file)
 
 print(f"Second KG MRR = {find_mrr(data)}")
@@ -53,7 +53,7 @@ print(f"Second KG Hits@{k2} = {find_hit_k(data, k2)}")
 print(f"Second KG Hits@{k3} = {find_hit_k(data, k3)}")
 print(f"Second KG Hits@{k4} = {find_hit_k(data, k4)}")
 
-with open('evaluation_results_bm25_on_third_KG.json', 'r') as file:
+with open('evaluation_results_bm25_KG3_b1.json', 'r') as file:
     data = json.load(file)
 
 print(f"Third KG MRR = {find_mrr(data)}")
@@ -62,8 +62,8 @@ print(f"Third KG Hits@{k2} = {find_hit_k(data, k2)}")
 print(f"Third KG Hits@{k3} = {find_hit_k(data, k3)}")
 print(f"Third KG Hits@{k4} = {find_hit_k(data, k4)}")
 
-print("==== embedding-based model b2====/n")
-with open('evaluation_results2_embedding-based_on_second_KG.json', 'r') as file:
+print("==== embedding-based model on second benchmark====/n")
+with open('evaluation_results_embedding-based_KG2_b2.json.json', 'r') as file:
     data = json.load(file)
 
 print(f"Second KG MRR = {find_mrr(data)}")
@@ -72,7 +72,7 @@ print(f"Second KG Hits@{k2} = {find_hit_k(data, k2)}")
 print(f"Second KG Hits@{k3} = {find_hit_k(data, k3)}")
 print(f"Second KG Hits@{k4} = {find_hit_k(data, k4)}")
 
-with open('evaluation_results2_embedding-based_on_third_KG.json', 'r') as file:
+with open('evaluation_results_embedding-based_KG3_b2.json.json', 'r') as file:
     data = json.load(file)
 
 print(f"Third KG MRR = {find_mrr(data)}")
@@ -82,7 +82,7 @@ print(f"Third KG Hits@{k3} = {find_hit_k(data, k3)}")
 print(f"Third KG Hits@{k4} = {find_hit_k(data, k4)}")
 
 print("==== BM25 model b2====/n")
-with open('evaluation_results2_bm25_on_second_KG.json', 'r') as file:
+with open('evaluation_results_bm25_KG2_b2.json', 'r') as file:
     data = json.load(file)
 
 print(f"Second KG MRR = {find_mrr(data)}")
@@ -91,7 +91,7 @@ print(f"Second KG Hits@{k2} = {find_hit_k(data, k2)}")
 print(f"Second KG Hits@{k3} = {find_hit_k(data, k3)}")
 print(f"Second KG Hits@{k4} = {find_hit_k(data, k4)}")
 
-with open('evaluation_results2_bm25_on_third_KG.json', 'r') as file:
+with open('evaluation_results_bm25_KG3_b2.json', 'r') as file:
     data = json.load(file)
 
 print(f"Third KG MRR = {find_mrr(data)}")
@@ -101,7 +101,7 @@ print(f"Third KG Hits@{k3} = {find_hit_k(data, k3)}")
 print(f"Third KG Hits@{k4} = {find_hit_k(data, k4)}")
 
 print("==== CEL-based 20-20 lp ====/n")
-with open('rag2_eval_second_kg.json', 'r') as file:
+with open('evalaluation_results_2nd_retrieval_method_KG2_20_20.json', 'r') as file:
     data = json.load(file)
 
 print(f"Second KG + CEL 20-20 MRR = {find_mrr(data)}")
@@ -110,7 +110,7 @@ print(f"Second KG + CEL 20-20 Hits@{k2} = {find_hit_k(data, k2)}")
 print(f"Second KG + CEL 20-20 Hits@{k3} = {find_hit_k(data, k3)}")
 print(f"Second KG + CEL 20-20 Hits@{k4} = {find_hit_k(data, k4)}")
 
-with open('rag2_eval_third_kg.json', 'r') as file:
+with open('evalaluation_results_2nd_retrieval_method_KG3_20_20.json', 'r') as file:
     data = json.load(file)
 
 print(f"Third KG + CEL 20-20 MRR = {find_mrr(data)}")
@@ -121,7 +121,7 @@ print(f"Third KG + CEL 20-20 Hits@{k4} = {find_hit_k(data, k4)}")
 
 print("==== CEL-based 10-10 lp ====/n")
 
-with open('rag2_eval_second_kg-10-10.json', 'r') as file:
+with open('evalaluation_results_2nd_retrieval_method_KG2_10_10.json', 'r') as file:
     data = json.load(file)
 
 print(f"Second KG + CEL 10-10 MRR = {find_mrr(data)}")
@@ -130,7 +130,7 @@ print(f"Second KG + CEL 10-10 Hits@{k2} = {find_hit_k(data, k2)}")
 print(f"Second KG + CEL 10-10 Hits@{k3} = {find_hit_k(data, k3)}")
 print(f"Second KG + CEL 10-10 Hits@{k4} = {find_hit_k(data, k4)}")
 
-with open('rag2_eval_third_kg-10-10.json', 'r') as file:
+with open('evalaluation_results_2nd_retrieval_method_KG3_10_10.json', 'r') as file:
     data = json.load(file)
 
 print(f"Third KG + CEL 10-10 MRR = {find_mrr(data)}")
@@ -141,7 +141,7 @@ print(f"Third KG + CEL 10-10 Hits@{k4} = {find_hit_k(data, k4)}")
 
 print("==== CEL-based 5-5 lp ====/n")
 
-with open('rag2_eval_second_kg-5-5.json', 'r') as file:
+with open('evalaluation_results_2nd_retrieval_method_KG2_5_5.json', 'r') as file:
     data = json.load(file)
 
 print(f"Second KG + CEL 5-5 MRR = {find_mrr(data)}")
@@ -150,7 +150,7 @@ print(f"Second KG + CEL 5-5 Hits@{k2} = {find_hit_k(data, k2)}")
 print(f"Second KG + CEL 5-5 Hits@{k3} = {find_hit_k(data, k3)}")
 print(f"Second KG + CEL 5-5 Hits@{k4} = {find_hit_k(data, k4)}")
 
-with open('rag2_eval_third_kg-5-5.json', 'r') as file:
+with open('evalaluation_results_2nd_retrieval_method_KG3_5_5.json', 'r') as file:
     data = json.load(file)
 
 print(f"Third KG + CEL 5-5 MRR = {find_mrr(data)}")
